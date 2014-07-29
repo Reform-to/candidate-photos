2867405830bd69448afe853b096326243ff6e05a
Christopher Adams <chris@raysend.com>
2014-07-29 20:58:52 +0800

    Add H4CA05257, H4FL02070, H4FL04043, etc.

--- js/candidates.js
+++ js/candidates.js
@@ -1,7 +1,61 @@
 window.ENV.ELECTIONS.candidates = [{
+  "fec_id": "H4CA05257",
+  "district": "05",
+  "state": "CA",
+  "full_name": "James Hinton",
+  "avatar": "/img/100x125/H4CA05257.jpg"
+},{
+  "fec_id": "H4FL02070",
+  "district": "02",
+  "state": "FL",
+  "full_name": "Luther Lee",
+  "avatar": "/img/100x125/H4FL02070.jpg"
+},{
+  "fec_id": "H4FL04043",
+  "district": "04",
+  "state": "FL",
+  "full_name": "Paula Darlene Moser-Bartlett",
+  "avatar": "/img/100x125/H4FL04043.jpg"
+},{
   "fec_id": "H4IA03065",
   "district": "03",
   "state": "IA",
   "full_name": "Staci Appel",
   "avatar": "/img/100x125/H4IA03065.jpg"
+},{
+  "fec_id": "H4MA06090",
+  "district": "06",
+  "state": "MA",
+  "full_name": "Seth Moulton",
+  "avatar": "/img/100x125/H4MA06090.jpg"
+},{
+  "fec_id": "H4NJ05135",
+  "district": "05",
+  "state": "NJ",
+  "full_name": "Roy Cho",
+  "avatar": "/img/100x125/H4NJ05135.jpg"
+},{
+  "fec_id": "H4NY07102",
+  "district": "07",
+  "state": "NY",
+  "full_name": "Jeffrey Kurzon",
+  "avatar": "/img/100x125/H4NY07102.jpg"
+},{
+  "fec_id": "H4UT01108",
+  "district": "01",
+  "state": "UT",
+  "full_name": "Craig Bowden",
+  "avatar": "/img/100x125/H4UT01108.jpg"
+},{
+  "fec_id": "H4WA08121",
+  "district": "08",
+  "state": "WA",
+  "full_name": "Jason Ritchie",
+  "avatar": "/img/100x125/H4WA08121.jpg"
+},{
+  "fec_id": "S4NH00112",
+  "district": "00",
+  "state": "NH",
+  "full_name": "Jim Rubens",
+  "avatar": "/img/100x125/S4NH00112.jpg"
 }];
